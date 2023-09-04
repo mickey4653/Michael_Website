7fc0ff85efd95805942baafadaa85db754cb9f9b
Michael Ilerioluwa Adeniyi <adeniyimichaelid@gmail.com>
2023-09-04 00:35:20 -0400

    updated Resume Website

---
+++ .vscode/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        ssr: {
+          // Example: Minify local SVG 
+          external: ["svgo"],
+        }
+      }
+});
